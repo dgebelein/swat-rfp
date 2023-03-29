6b2b20e42d0fa7ff1cec8087429285bb9a0766e7
dgebelein <93367973+dgebelein@users.noreply.github.com>
2023-03-29 17:33:34 +0200

    Add files via upload

---
+++ Swop/glob/Instructions.cs
@@ -0,0 +1,326 @@
+﻿//using Swop.defs;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using swatSim;
+
+namespace Swop.glob
+
+{
+	class Instructions
+	{
+		public List<string> WeatherFilenames;
+		public List<string> MonitoringFilenames;
+		public List<string> LocParamFilenames;
+		public List<string> EvalDates;
+		public String GlobDate;
+
+		public List<int> FirstIndices;
+		public List<int> LastIndices;
+		public List<double> EvalWeightings;
+
+		public List<string> ParameterKeys;
+		public List<string> ParameterInitials;
+
+		public FlyType ModelTyp;
+		public SwopWorkMode SwopMode;
+		public string Description;
+		public int FirstOptIndex;
+		public int LastOptIndex;
+
+		public string ErrMsg;
+		public int ErrLineNo;
+
+		public Instructions()
+		{
+			WeatherFilenames = new List<string>();
+			MonitoringFilenames = new List<string>();
+			LocParamFilenames = new List<string>();
+			EvalDates = new List<string>();
+
+
+			FirstIndices = new List<int>();
+			LastIndices = new List<int>();
+			ParameterKeys = new List<string>();
+			ParameterInitials = new List<string>();
+
+			EvalWeightings = new List<double>();
+
+		}
+
+		public int NumLocations
+		{
+			get { return WeatherFilenames.Count;}
+		}
+
+
+		private string IsolateContent(string sourceLine,string key)
+		{
+			string source = sourceLine.ToLower();
+			if (source.ToLower().Contains(key))
+			{
+				int startPos = source.IndexOf(key, 0) + key.Length;
+
+				int endPos = source.IndexOf("#", startPos);
+				if(endPos <=0 )
+					endPos = source.Length;
+
+				return (source.Substring(startPos, endPos - startPos).Trim());
+
+			}
+			else
+				return null;
+
+		}
+
+		private void ReadSet(string line)
+		{
+			string weatherfile = IsolateContent(line, "#w:");
+			if (weatherfile== null)
+			{
+				ErrMsg = "Wetterdaten fehlen"+ " (" + line + ")";
+				return;
+			}
+			WeatherFilenames.Add(weatherfile);
+
+			string monitoringfile = IsolateContent(line, "#m:");
+			if (monitoringfile == null)
+			{
+				ErrMsg = "Monitoringdaten fehlen" + " (" + line + ")";
+				return;
+			}
+			MonitoringFilenames.Add(monitoringfile);
+
+			string paramsfile = IsolateContent(line, "#p:");
+			if (paramsfile != null)
+			{
+				LocParamFilenames.Add(paramsfile);
+			}
+			else
+				LocParamFilenames.Add("");
+
+
+			//string datesString = IsolateContent(line, "#d:");
+			//if (datesString != null)
+			//{
+			//	Tuple<int, int> indices = GetLocIndices(datesString);
+			//	if ((indices.Item1 < 0) || (indices.Item2 < 0))
+			//	{
+			//		ErrMsg = ErrMsg = "falsche Zeitbegrenzung" + " (" + line + ")";
+			//		return;
+			//	}
+			//	else
+			//	{
+			//		FirstIndices.Add(indices.Item1);
+			//		LastIndices.Add(indices.Item2);
+			//	}
+			//}
+			//else
+			//{
+			//	FirstIndices.Add(-1);
+			//	LastIndices.Add(-1);
+			//}
+
+
+			string datesString = IsolateContent(line, "#d:");
+			if (datesString != null)
+			{
+				EvalDates.Add(datesString);
+			}
+			else
+			{
+				EvalDates.Add("");
+			}
+
+			string weightString = IsolateContent(line, "#e:");
+			if (weightString != null)
+			{
+				if(double.TryParse(weightString, NumberStyles.Number, CultureInfo.InvariantCulture, out double d))
+				{
+					if ((d >= 0.0) && (d <= 10.0))
+						EvalWeightings.Add(d);
+					else
+						ErrMsg = ErrMsg = "Gewichtungfaktor muss zwischen 0 und 10.0 liegen" + " (" + line + ")";
+				}
+				else
+				{
+					ErrMsg = ErrMsg = "Gewichtungfaktor fehlerhaft" + " (" + line + ")";
+				}
+
+			}
+			else
+			{
+				EvalWeightings.Add(1.0);
+			}
+		}
+
+		private void ReadDescription(string line)
+		{
+			Description = line.Trim();
+		}
+
+		private void ReadModelType(string line)
+		{
+			if (string.IsNullOrEmpty(line))
+			{
+				ErrMsg = "Modell-Angabe fehlt";
+				return;
+			}
+			foreach(FlyType m in Enum.GetValues(typeof(FlyType)))
+			{
+				if (string.Compare(line.Trim(), m.ToString(), true) == 0)
+				{ 
+					ModelTyp = m;
+					return;
+				}
+			}
+			ErrMsg = "Modell-Angabe falsch";
+		}
+
+		private void ReadSwopMode(string line)
+		{
+			if (string.IsNullOrEmpty(line))
+			{
+				ErrMsg = "SwopMode-Angabe fehlt";
+				return;
+			}
+			foreach (SwopWorkMode m in Enum.GetValues(typeof(SwopWorkMode)))
+			{
+				if (string.Compare(line.Trim(), m.ToString(), true) == 0)
+				{
+					SwopMode = m;
+					return;
+				}
+			}
+			ErrMsg = "SwopMode-Angabe falsch";
+		}
+
+		private void ReadParam(string line)
+		{
+			if (string.IsNullOrEmpty(line))
+			{
+				ErrMsg = "Parameter-Angabe fehlt";
+				return;
+			}
+			ParameterKeys.Add(line);
+		}
+
+		private void ReadInitialParam(string line)
+		{
+			if (string.IsNullOrEmpty(line))
+			{
+				ErrMsg = "Parameter-Angabe fehlt";
+				return;
+			}
+			ParameterInitials.Add(line);
+		}
+
+		private void ReadDate(string line)
+		{
+			string[] elems = line.Trim().Split('-');
+
+			if (elems.Length != 2)
+			{
+				ErrMsg = "Datumsangabe falsch";
+				return;
+			}
+
+			if (DateTime.TryParseExact(elems[0].Trim(), "dd.MM", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime first) &&
+				DateTime.TryParseExact(elems[1].Trim(), "dd.MM", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime last))
+			{
+
+				GlobDate = line;
+				//FirstOptIndex = first.DayOfYear - 1;
+				//LastOptIndex = last.DayOfYear - 1;
+			}
+			else
+			{
+				ErrMsg = "'Date=' Datumsangabe falsch";
+			}
+		}
+
+		//private Tuple<int,int>GetLocIndices(string line)
+		//{
+		//	Tuple<int, int> empty = new Tuple<int, int>(-1, -1);
+		//	if (string.IsNullOrEmpty(line))
+		//		return empty;
+		//	else
+		//	{
+		//		string[] elems = line.Trim().Split('-');
+
+		//		if (elems.Length != 2)
+		//		{
+		//			ErrMsg = "falsche Zeitbegrenzung";
+		//			return empty; 
+		//		}
+
+		//		if (DateTime.TryParseExact(elems[0].Trim(), "dd.MM", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime first) &&
+		//			DateTime.TryParseExact(elems[1].Trim(), "dd.MM", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime last))
+		//		{
+		//			return new Tuple<int, int>(first.DayOfYear-1, last.DayOfYear - 1);
+		//			//return new Tuple<int, int>(first.DayOfYear, last.DayOfYear);
+
+		//		}
+		//		else
+		//		{
+		//			ErrMsg = "falsche Zeitbegrenzung";
+		//			return empty;
+		//		}
+		//	}
+		//}
+
+
+		public bool Read(string filename)
+		{
+			ErrMsg = "";
+			ErrLineNo = 0;
+			SwopMode = SwopWorkMode.LEAST;
+
+			try
+			{
+				using (StreamReader sr = new StreamReader(filename, Encoding.UTF8, true))
+				{
+					string line;
+					while ((line = sr.ReadLine()) != null)
+					{
+						if (line.Trim().StartsWith("--"))// Kommentar
+							continue;
+
+						ErrLineNo++;
+						string[] elems = line.Trim().Split('=');
+						if (elems.Length == 2)
+						{
+							switch (elems[0].Trim().ToLower())
+							{
+								case "swopmode": ReadSwopMode(elems[1]); break;
+								case "descr": ReadDescription(elems[1]); break;
+								case "model": ReadModelType(elems[1]); break;
+								case "set": ReadSet(elems[1]); break;
+								case "param": ReadParam(elems[1]); break;
+								case "init": ReadInitialParam(elems[1]); break;
+								case "date": ReadDate(elems[1]); break;
+								default: ErrMsg = "unbekannte Anweisung: '" + line + "'"; break;
+							}
+							if (!string.IsNullOrEmpty(ErrMsg))
+							{
+								ErrMsg = $"Zeile: { ErrLineNo}: {ErrMsg}";
+								return false;
+							}
+						}
+					}
+				}
+
+			}
+			catch(Exception e)
+			{
+				ErrMsg = "Fehler: " + e.Message;
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ Swop/glob/VmSwop.cs
@@ -0,0 +1,241 @@
+﻿using Microsoft.Win32;
+//using Swop.data;
+using Swop.optimizer;
+//using Swop.sim;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using TTP.UiUtils;
+using swatSim;
+using System.Windows;
+
+namespace Swop.glob
+{
+	public class VmSwop :INotifyPropertyChanged
+	{
+
+		//GlobData SwopData;
+		RelayCommand _openCommand;
+		RelayCommand _abortCommand;
+		RelayCommand _executeCommand;
+
+		MultiOptimizer _optimizer;
+		MultiCombiner  _combiner;
+
+
+		bool _canExecute;
+		bool _isExecutionRunning;
+		public GlobData SwopData { get; private set; }
+
+
+		public VmSwop()
+		{
+			_openCommand = new RelayCommand(param => this.OpenCommandFile(), param => this.CanOpenCommandFile);
+			_abortCommand = new RelayCommand(param => this.AbortExecution(), param => this.CanAbortExecution);
+			_executeCommand = new RelayCommand(param => this.ExecuteOptimization(), param => this.CanExecuteOptimization);
+			SwopData = new GlobData();
+		}
+
+		public ICommand OpenCommand { get { return _openCommand; } }
+		public ICommand AbortCommand { get { return _abortCommand; } }
+		public ICommand ExecuteCommand { get { return _executeCommand; } }
+		// Binding UI
+		public string CmdName{get { return SwopData.CmdFilename; }}
+		public string CmdErrorText	{	get { return SwopData.ErrorMessage; }}
+		public string BestText{	get { return SwopData.BestText; }}
+		public string PrologText{	get {return SwopData.PrologText;}}
+
+		public Visibility VisOptimization 
+		{	
+			get { if ((SwopData.WorkMode == SwopWorkMode.LEAST) || (SwopData.WorkMode == SwopWorkMode.SHRINK))
+						return Visibility.Visible;
+					else
+						return Visibility.Collapsed; 
+			}	
+		}
+
+		public Visibility VisCombination { get { return (SwopData.WorkMode == SwopWorkMode.COMBI) ? Visibility.Visible : Visibility.Collapsed; } }
+		public string OptStep{ get { return SwopData.OptStep; }}
+		public string StepEval { get { return SwopData.StepEval; } }
+		public string OptLap { get { return SwopData.OptLap; } }
+		public string LapEval { get { return SwopData.LapEval; } }
+		public string EndText { get { return SwopData.EndText; } }
+		public string RemainingSteps { get { return SwopData.RemainingSteps; } }
+		public string TotalBestEval { get { return SwopData.TotalBestEval; } }
+		public string CombiStep { get { return SwopData.CombiStep; } }
+		public string CombiStepsRemaining { get { return SwopData.CombiStepsRemaining; } }
+
+
+
+		bool CanAbortExecution
+		{
+			get { return _isExecutionRunning; }
+		}
+
+		void AbortExecution()
+		{
+			if ((SwopData.WorkMode == SwopWorkMode.LEAST) || (SwopData.WorkMode == SwopWorkMode.SHRINK))
+				_optimizer.AbortExecution();
+			else
+				_combiner.AbortExecution();
+
+			_isExecutionRunning = false;
+		}
+
+		bool CanExecuteOptimization
+		{
+			get { return _canExecute && !_isExecutionRunning; }
+		}
+
+		void ExecuteOptimization()
+		{
+			if ((SwopData.WorkMode == SwopWorkMode.LEAST) || (SwopData.WorkMode == SwopWorkMode.SHRINK))
+				_optimizer.ExecuteOptimization();
+			else
+				_combiner.ExecuteCombination();
+
+			_isExecutionRunning = true;
+		}
+
+		bool CanOpenCommandFile
+		{
+			get { return !_isExecutionRunning; }
+		}
+
+		void OpenCommandFile()
+		{
+			OpenFileDialog dlg = new OpenFileDialog
+			{
+				InitialDirectory = SwopData.GetPathSwop,
+				Filter = "Swop files (*.swop)|*.swop|All files (*.*)|*.*"
+			};
+
+			if (dlg.ShowDialog()== true)
+			{
+				_canExecute = SwopData.ReadInstructions(dlg.FileName);
+
+				//if (SwopData.SwopMode == SwopWorkMode.OPTI)
+				//	_optimizer = new MultiOptimizer(SwopData, OptimizerProgressChanged, OptimizerCompleted);
+				//else
+				//	_combiner = new MultiCombiner(SwopData, CombinerProgressChanged, CombinerCompleted);
+
+				if (_canExecute)
+				{
+					if ((SwopData.WorkMode == SwopWorkMode.LEAST) || (SwopData.WorkMode == SwopWorkMode.SHRINK))
+					{
+						_optimizer = new MultiOptimizer(SwopData, OptimizerProgressChanged, OptimizerCompleted);
+						SwopData.CreatePrologDisplayText(_optimizer);
+					}
+					else { 
+						_combiner = new MultiCombiner(SwopData, CombinerProgressChanged, CombinerCompleted);
+						SwopData.CreatePrologDisplayText(_combiner);
+					}
+
+					SwopData.WritePrologLogText();
+					SwopData.EndText = "";
+				}
+				else
+					SwopData.PrologText = SwopData.ErrorMessage;
+			}
+
+			OnPropertyChanged("CmdName");
+			OnPropertyChanged("BestText");
+			OnPropertyChanged("PrologText");
+			OnPropertyChanged("EndText");
+			OnPropertyChanged("VisOptimization");
+			OnPropertyChanged("VisCombination");
+
+
+		}
+
+		#region Backgroundworker-Methoden
+
+		void OptimizerProgressChanged(object sender, ProgressChangedEventArgs e)
+		{
+			if (e.ProgressPercentage == 0)
+			{ 
+				OnPropertyChanged("PrologText");
+				return;
+			}
+
+			if (e.ProgressPercentage == 50)
+			{
+					OnPropertyChanged("OptStep");
+					OnPropertyChanged("StepEval");
+					OnPropertyChanged("OptLap");
+					OnPropertyChanged("LapEval");
+					OnPropertyChanged("RemainingSteps");
+					OnPropertyChanged("TotalBestEval");
+					OnPropertyChanged("BestText");
+
+				return;
+			}
+
+			if (e.ProgressPercentage == 100)
+			{
+				OnPropertyChanged("EndText");
+			}
+		}
+
+		void OptimizerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			_isExecutionRunning = false;
+			CommandManager.InvalidateRequerySuggested();
+		}
+
+		void CombinerProgressChanged(object sender, ProgressChangedEventArgs e)
+		{
+			if (e.ProgressPercentage == 0)
+			{
+				OnPropertyChanged("PrologText");
+				return;
+			}
+
+			if (e.ProgressPercentage == 50)
+			{
+				OnPropertyChanged("CombiStep");
+				OnPropertyChanged("StepEval");
+				OnPropertyChanged("CombiStepsRemaining");
+				return;
+			}
+
+			if (e.ProgressPercentage == 100)
+			{
+				OnPropertyChanged("EndText");
+			}
+
+		}
+
+		void CombinerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			_isExecutionRunning = false;
+			CommandManager.InvalidateRequerySuggested();
+		}
+
+		#endregion
+
+		#region INotifyPropertyChanged Members
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			try
+			{
+				PropertyChangedEventHandler handler = this.PropertyChanged;
+				if (handler != null)
+					handler(this, new PropertyChangedEventArgs(propertyName));
+			}
+			catch { }// wegen   Tooltip-Fehler: so wird exception abgefangen - aber tooltip bleibt leer
+
+
+		}
+
+		#endregion
+
+	}
+}
